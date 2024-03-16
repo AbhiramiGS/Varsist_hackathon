aa99cda54b2de94ef0971cace3751656a8777869
Roshan Jagadish <roshan10.rj@gmail.com>
2024-03-16 14:43:35 +0530

    added location router

--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -1,9 +1,12 @@
+import { aqRouter } from "./router/aq";
+import { locationRouter } from "./router/location";
 import { springRouter } from "./router/spring";
 import { createTRPCRouter } from "./trpc";
 
 export const appRouter = createTRPCRouter({
-
-  spring: springRouter
+  spring: springRouter,
+  location: locationRouter,
+  aq: aqRouter,
 });
 
 // export type definition of API
---
+++ packages/api/src/router/location.ts
@@ -0,0 +1,22 @@
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure } from "../trpc";
+
+export const locationRouter = createTRPCRouter({
+  getLatestLocation: publicProcedure.query(({ ctx }) => {
+    return ctx.db.location.findFirst({
+      orderBy: { createdAt: "desc" },
+    });
+  }),
+
+  findLocation: publicProcedure
+    .input(z.object({ latitude: z.number(), longitude: z.number() }))
+    .query(({ ctx, input }) => {
+      return ctx.db.location.findFirst({
+        where: {
+          latitude: input.latitude,
+          longitude: input.longitude,
+        },
+      });
+    }),
+});
